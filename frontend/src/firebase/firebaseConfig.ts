4219cfbc50d07a9924ab3f04a8d7eb3ff7d3c406
Crocohack <njannahzmn0208@gmail.com>
2026-02-28 23:16:07 +0800

    fix commit

--- frontend/src/firebase/firebaseConfig.ts
+++ frontend/src/firebase/firebaseConfig.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyDma-bec1DkHPOvBXn-mMSvZt7tKCssbWU",
-  authDomain: "sedia-banjir-2026-7fe9c.firebaseapp.com",
-  projectId: "sedia-banjir-2026-7fe9c",
-  storageBucket: "sedia-banjir-2026-7fe9c.firebasestorage.app",
-  messagingSenderId: "1075618835063",
-  appId: "1:1075618835063:web:17a485ef07f2837e93b472",
-  measurementId: "G-N1GQTYF47R"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
