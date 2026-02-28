572e67b655ea22719666e77f1383aba118dbd985
Crocohack <njannahzmn0208@gmail.com>
2026-02-28 18:55:28 +0800

    change

--- frontend/src/firebase/firebaseConfig.ts
+++ frontend/src/firebase/firebaseConfig.ts
@@ -4,6 +4,6 @@ export const firebaseConfig = {
   projectId: "sedia-banjir-2026-7fe9c",
   storageBucket: "sedia-banjir-2026-7fe9c.firebasestorage.app",
   messagingSenderId: "1075618835063",
-  appId: "1:1075618835063:web:357dc100f9e8eddb93b472",
-  measurementId: "G-B7QTPFCNTC"
+  appId: "1:1075618835063:web:17a485ef07f2837e93b472",
+  measurementId: "G-N1GQTYF47R"
 };
